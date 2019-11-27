e6d6cf02ef06b3075ece8d1000db1a5876d8ab71
VivienFreb <vivien.frebourg@outlook.com>
2019-11-27 20:58:45 +0100

    Prise en compte de l'ancre déjà présente dans l'url +re-style

--- onglet/tabs.js
+++ onglet/tabs.js
@@ -1,10 +1,9 @@
-var tabs = document.querySelectorAll('.tabs a')
-for(var i = 0; i < tabs.length; i++){
-    tabs[i].addEventListener('click', function(e){
-        var li = this.parentNode;
-        var div = this.parentNode.parentNode.parentNode
+(function () {
+    var showTab = function (a) {
+        var li = a.parentNode;
+        var div = a.parentNode.parentNode.parentNode
 
-        if(li.classList.contains('active')){
+        if (li.classList.contains('active')) {
             return false
         }
 
@@ -12,7 +11,22 @@ for(var i = 0; i < tabs.length; i++){
         li.classList.add('active')
 
         div.querySelector('.tab-content.active').classList.remove('active')
-        div.querySelector(this.hash).classList.add('active')
+        div.querySelector(a.getAttribute('href')).classList.add('active')
+    }
 
-    })
-}
+    var tabs = document.querySelectorAll('.tabs a')
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].addEventListener('click', function (e) {
+            showTab(this)
+        })
+    }
+
+// Permet de récupérer l'ancre qui est présente dans l'url pour lui appliquer directement la classe active.
+    var hash = window.location.hash
+    var a = document.querySelector('a[href="' + hash + '"]')
+// Si l'ancre dans l'url correspond bien a quelque chose (!== null) et qu'elle ne possède pas la classe active, alors on
+// effectue l'action permettant de lui attribuer cette classe
+    if (a !== null && !a.parentNode.classList.contains('active')) {
+        showTab(a)
+    }
+})()
