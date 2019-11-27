74c46c2b40844064ed115365f818b1f89904314b
VivienFreb <vivien.frebourg@outlook.com>
2019-11-27 16:17:58 +0100

    Amélioration: Cacher un élément en lui ajoutant la classe "spoiler"

--- spoiler/spoiler.js
+++ spoiler/spoiler.js
@@ -1,5 +1,36 @@
-var button = document.querySelector('.spoiler-container button')
-button.addEventListener('click', function(){
-    this.nextElementSibling.classList.add('visible')
-    button.parentNode.removeChild(this)
-})
+// // Première version simpliste, qui demande de faire plusieurs balises en html
+// var button = document.querySelector('.spoiler-container button')
+// button.addEventListener('click', function(){
+//     this.nextElementSibling.classList.add('visible')
+//     button.parentNode.removeChild(this)
+// })
+
+// Cette version permet de simplement donner une classe "spoiler" a un element pour le cacher.
+(function(){
+    var createSpoilerButton = function(element){
+        var button = document.createElement('button')
+        var spoilerContent = document.createElement('span')
+
+        spoilerContent.className = 'spoiler-content'
+        spoilerContent.innerHTML = element.innerHTML
+
+        button.textContent = 'Afficher le spoiler'
+        element.innerHTML = ''
+
+        element.appendChild(button)
+        element.appendChild(spoilerContent)
+
+        button.addEventListener('click', function(){
+            this.nextElementSibling.classList.add('visible')
+            button.parentNode.removeChild(this)
+        })
+
+    }
+
+    var spoilers = document.querySelectorAll('.spoiler')
+    for (var i = 0; i < spoilers.length; i++){
+        createSpoilerButton(spoilers[i])
+    }
+})()
+
+
